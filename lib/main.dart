4c81440f6a5ef8423100bb0723dfdfe9b803785d
unknown <mysample@example.com>
2023-06-14 22:48:23 +0900

    Main format

--- lib/main.dart
+++ lib/main.dart
@@ -4,65 +4,51 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _currentPageIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+      theme: ThemeData(useMaterial3: true),
+      home: Scaffold(
+        body: const [
+          Center(
+            child: Text('Home'),
+          ),
+          Center(
+            child: Text('Timetable'),
+          ),
+          Center(
+            child: Text('Search'),
+          ),
+          Center(
+            child: Text('More'),
+          )
+        ][_currentPageIndex],
+        bottomNavigationBar: NavigationBar(
+          selectedIndex: _currentPageIndex,
+          destinations: const <NavigationDestination>[
+            NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+            NavigationDestination(
+                icon: Icon(Icons.date_range), label: 'Timetable'),
+            NavigationDestination(icon: Icon(Icons.search), label: 'Search'),
+            NavigationDestination(icon: Icon(Icons.menu), label: 'More'),
           ],
+          onDestinationSelected: (value) {
+            setState(() {
+              _currentPageIndex = value;
+            });
+          },
+          labelBehavior: NavigationDestinationLabelBehavior.alwaysShow,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
